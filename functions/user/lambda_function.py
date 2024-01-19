afd9b8639c9838bae28241c4c82c635eb37c0aec
1337kid <45532566+1337kid@users.noreply.github.com>
2024-01-19 21:04:12 +0530

    try except

--- functions/user/lambda_function.py
+++ functions/user/lambda_function.py
@@ -9,19 +9,25 @@ app = firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 def lambda_handler(event, context):
-    uid = event['uid']
-    user_record = db.collection('users-test').where(filter=FieldFilter('uid','==',uid)).get()
-    if user_record:
-        user_record_dict = user_record[0].to_dict()
-        user_templates = user_record_dict['templates']
-        if user_templates:
-            for template in user_templates:
-                if template['default_template'] == True:
-                    return {
-                        'statusCode': 200,
-                        'body': json.dumps(template)
-                    }
-    return {
-        'statusCode': 404,
-        'body': json.dumps('User not found')
-    }
+    try:
+        uid = event['uid']
+        user_record = db.collection('users-test').where(filter=FieldFilter('uid','==',uid)).get()
+        if user_record:
+            user_record_dict = user_record[0].to_dict()
+            user_templates = user_record_dict['templates']
+            if user_templates:
+                for template in user_templates:
+                    if template['default_template'] == True:
+                        return {
+                            'statusCode': 200,
+                            'body': json.dumps(template)
+                        }
+        return {
+            'statusCode': 404,
+            'body': json.dumps('User not found')
+        }
+    except:
+        return {
+            'statusCode': 500,
+            'body': json.dumps('Internal Server Error')
+        }
